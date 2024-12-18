c0247322c31c578879d59e097232cc3376ea45da
M-Sami-Alesh <42234447+Sami-AlEsh@users.noreply.github.com>
2024-12-18 19:54:06 +0400

    Add swagger

--- config/env.schema.ts
+++ config/env.schema.ts
@@ -2,4 +2,5 @@ import Joi from 'joi';
 
 export const envValidationSchema = Joi.object({
   NODE_ENV: Joi.string().valid('develop', 'production'),
+  PORT: Joi.number().default(3000),
 });
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CustomLogger } from './common/helpers/custom.logger';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new CustomLogger(),
   });
 
+  const configService = app.get(ConfigService);
+
+  if (configService.get<string>('NODE_ENV') !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Weather App')
+      .setDescription('The Weather App API description')
+      .setVersion('1.0')
+      .build();
+
+    SwaggerModule.setup('api', app, () =>
+      SwaggerModule.createDocument(app, config),
+    );
+  }
+
   await app.listen(3000);
+
+  console.log(
+    `\n>> Swagger UI is available at http://localhost:${configService.get<number>('PORT')}/api\n`,
+  );
 }
 bootstrap();
