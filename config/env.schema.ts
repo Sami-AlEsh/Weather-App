5da564fc64e68751843fafaa43fbb38eb89e4bd5
M-Sami-Alesh <42234447+Sami-AlEsh@users.noreply.github.com>
2024-12-18 19:39:25 +0400

    Setup project + libraries

---
+++ config/env.schema.ts
@@ -0,0 +1,5 @@
+import Joi from 'joi';
+
+export const envValidationSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('develop', 'production'),
+});
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import { envValidationSchema } from 'config/env.schema';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: envValidationSchema,
+    }),
+  ],
 })
 export class AppModule {}
